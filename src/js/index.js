7174535f16167043286eb3568aa9742a2ec999c1
Olena <helensmart2323@gmail.com>
2023-09-29 20:33:33 +0200

    1

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,16 @@
 import axios from 'axios';
-
+import SlimSelect from 'slim-select';
 import { fetchBreeds, fetchCatByBreed } from './cat-api';
+import Notiflix from 'notiflix';
 
 const selectEl = document.querySelector('.breed-select');
 const loaderEl = document.querySelector('.loader');
 const errorEl = document.querySelector('.error');
 const divEl = document.querySelector('.cat-info');
+
+new SlimSelect({
+  select: '.breed-select',
+});
 
 loaderEl.classList.add('is-hidden');
 errorEl.classList.add('is-hidden');
@@ -20,10 +25,13 @@ fetchBreeds()
     selectEl.innerHTML = markup;
   })
   .catch(err => {
+    errorEl.classList.remove('is-hidden');
     console.log(err);
   });
 
 function onChange() {
+  loaderEl.classList.remove('is-hidden');
+  errorEl.classList.add('is-hidden');
   const selectedCat = selectEl.value;
   console.log(selectedCat);
   fetchCatByBreed(selectedCat)
@@ -31,14 +39,16 @@ function onChange() {
       renderPage(info);
     })
     .catch(err => {
+      errorEl.classList.remove('is-hidden');
       console.log(err);
     });
 }
 
 function renderPage(data) {
+  loaderEl.classList.add('is-hidden');
   const markup = `
-    <img src="${data[0].url}" alt="cat" width = "800">
     <h2>${data[0].breeds[0].name}</h2>
+    <img src="${data[0].url}" alt="cat" width = "800">
     <p>${data[0].breeds[0].description}</p>
     <p>${data[0].breeds[0].temperament}</p>
     `;
